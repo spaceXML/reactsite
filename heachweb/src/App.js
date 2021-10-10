01e5028f2734d44ce7c60e67b381cc08dd35e886
spaceXML <84154282+spaceXML@users.noreply.github.com>
2021-10-10 16:03:43 +0900

    2021-10-10
    2021-10-10

---
+++ heachweb/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+    <p> 안녕하십니까?</p>
+    <button primary>하이하이1</button>
+    </div>
+  );
+}
+
+export default App;
